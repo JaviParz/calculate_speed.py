5d4913f7b8a0cedb8360821bb35d482f8585c62a
JaviParz <125891126+JaviParz@users.noreply.github.com>
2023-04-18 14:01:53 +0300

    Create calculate_speed.py

---
+++ calculate_speed.py
@@ -0,0 +1,5 @@
+print("Эта программа рассчитывает скорость.")
+distance = float(input("Введите расстояние (в км): "))
+time = float(input("Введите время (в часах): "))
+speed = distance / time
+print("Скорость равна:", speed, "км/ч")
